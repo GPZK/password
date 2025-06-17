2777249110061ac9681e695b06319fb6d91150f1
Nikita <roman.komikita@gmail.com>
2025-06-17 13:11:55 +0300

    2 edition

--- password.py
+++ password.py
@@ -2,30 +2,31 @@ import argparse
 import random
 
 
-elementPool = list('qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM')
+element_pool = list('qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM')
+symbols = list('\\\'~`! @#$%^&*()_-+={[}]|:;"<,>.?/')
+numbers = list('0123456789')
 length = 0
 
-def parseParam():
-    global length
-    symbols = list('\\\'~`! @#$%^&*()_-+={[}]|:;"<,>.?/')
-    numbers = list('0123456789')
-
+def setup_parser():
     parser = argparse.ArgumentParser()
     parser.add_argument('length', help='type the number of symbols in your desired password', type=int)
     parser.add_argument('-s', help = 'type this to include special symbols', action = 'store_true')
     parser.add_argument('-n', help = 'type this to include numbers', action = 'store_true')
+    return parser
+
+def parse_param(parser):
     args = parser.parse_args()
+    return args
 
+def generatePassword(args):
     if (args.s):
-        elementPool.extend(symbols)
+        element_pool.extend(symbols)
     if (args.n):
-        elementPool.extend(numbers)
-    length = args.length
-
-def generatePassword(pool, len):
-    password = ''.join(random.choices(pool, k = len))
+        element_pool.extend(numbers)
+    password = ''.join(random.choices(element_pool, k = args.length))
     return password
 
 if __name__ ==  '__main__':
-    parseParam()
-    print(generatePassword(elementPool, length))
+    parser = setup_parser()
+    args = parse_param(parser)
+    print(generatePassword(args))
